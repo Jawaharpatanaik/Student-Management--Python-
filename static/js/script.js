ebf97ebd9027b13e170d4acddb275292c3e14ced
Jawaharpatanaik <jawaharpatanaik@gmail.com>
2025-11-21 11:22:22 +0530

    added Css and Javascript

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// General JS file
+console.log("Student Management System Loaded Successfully");
+
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this record?");
+}
